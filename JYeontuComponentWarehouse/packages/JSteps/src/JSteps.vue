d33fcd6e4e8ca8f98ce6e47fc1a31f853c54bbb2
JYeontu <yongtaozheng0201@163.com>
2021-11-29 17:56:05 +0800

    步骤条init

---
+++ JYeontuComponentWarehouse/packages/JSteps/src/JSteps.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="j-steps">
+		
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"JSteps",
+		props:{
+			direction:{
+				type:String,
+				default:'horizontal'//horizontal 横向 vertical 纵向
+			}
+		},
+		data() {
+			return{
+				showTab:0
+			}
+		},
+		created() {
+			this.init();
+		},
+		methods:{
+			init(){
+				this.showTab = this.defaultShowTab;
+			},
+			barClick(index){
+				this.showTab = index;
+				this.$emit('clickTab',index);
+			},
+			getBarClass(index,val=''){
+				let res = '';
+				if(index == this.showTab){
+					res += 'active-tab';
+				}
+				return res + ' ' + val;
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped="scoped">
+	
+</style>
